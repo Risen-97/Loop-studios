281c10fd671f2a9fd36618c479db5d843fc23c91
Risen-97 <m.fareeed1997@gmail.com>
2022-03-30 01:32:53 +0300

    fixed scroll bug and add a intersection api

--- js/app.js
+++ js/app.js
@@ -1,17 +1,36 @@
 const menuBtn = document.querySelector(".nav__menu");
 const nav = document.querySelector(".nav");
 const hero = document.querySelector(".hero");
+const scrollBtn = document.querySelector(".scroll__top");
 
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle("close");
 });
 
-const fixedHeight = hero.getBoundingClientRect().height - 80;
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
-window.addEventListener("scroll", () => {
-  if (window.scrollY > fixedHeight) {
+const fixedNav = (entries) => {
+  [entry] = entries;
+
+  if (!entry.isIntersecting) {
     nav.classList.add("fixed");
+    scrollBtn.classList.add("show");
   } else {
     nav.classList.remove("fixed");
+    scrollBtn.classList.remove("show");
   }
-});
+};
+
+const fixedOption = {
+  root: null,
+  threshold: 0.2,
+};
+
+const navObserver = new IntersectionObserver(fixedNav, fixedOption);
+
+navObserver.observe(hero);
