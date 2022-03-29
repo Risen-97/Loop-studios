fe1637da2a4c8507230edef924e275d2c634e0b7
Risen-97 <m.fareeed1997@gmail.com>
2022-03-30 00:23:11 +0300

    added a footer and sticky nav

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 const menuBtn = document.querySelector(".nav__menu");
 const nav = document.querySelector(".nav");
+const hero = document.querySelector(".hero");
 
 menuBtn.addEventListener("click", () => {
   nav.classList.toggle("close");
+});
+
+const fixedHeight = hero.getBoundingClientRect().height - 80;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > fixedHeight) {
+    nav.classList.add("fixed");
+  } else {
+    nav.classList.remove("fixed");
+  }
 });
